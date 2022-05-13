94d84623d83260b7a970e134851fa6aa1cdb0e4c
javaria105 <55933082+javaria105@users.noreply.github.com>
2022-05-13 11:48:36 -0500

    Create random-games.py

---
+++ random-games.py
@@ -0,0 +1,10 @@
+#!/bin/python
+
+from random import choice
+
+
+# create list of games.
+games = ['League', 'Valorant', 'Chicken Horse', 'Devour']
+
+# print a random game from games.
+print(choice(games))
